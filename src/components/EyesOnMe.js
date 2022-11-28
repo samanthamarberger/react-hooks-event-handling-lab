896f6ce8cd5b8c7fde468fdc359565787f9252cb
samanthamarberger <slatham04@gmail.com>
2022-11-28 13:50:51 -0700

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    return <button onBlur={()=> console.log('Good!')} onFocus={()=> console.log("Hey! Eyes on me!")}>Eyes on me</button>
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input
+                type="text"
+                name="password"
+                onChange={handleChange}
+                placeholder="Enter password..."
+            />
+        </div>
     )
+}
+
+function handleChange() {
+    console.log('Entering password...')
 }
 
 export default Keypad;
